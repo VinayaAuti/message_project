6bc78dd9c462f9f05b40a1c992916d703c0b61d1
VinayaAuti <vinaya.badhe@gmail.com>
2023-11-11 18:05:01 +0530

    first commit

---
+++ message_app/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render,HttpResponse,redirect
+from .models import Msg
+
+# Create your views here.
+def create(request):
+    if request.method=='POST':
+        #print("request is :", request.method)
+        #fetch date
+        #pass
+        n=request.POST['uname']
+        mail=request.POST['uemail']
+        mob=request.POST['mobile']
+        msg=request.POST['msg']
+        m=Msg.objects.create(name=n,email=mail,mobile=mob,msg=msg)
+        m.save()
+        #print(n,"-",mail,"-",mob,"-",msg)
+        return redirect('/dashboard')
+        #return HttpResponse("data inserted successfully")
+    else:   
+       # print("request is :", request.method)
+        return render(request,'create.html')
+
+def dashboard(request):
+    m=Msg.objects.all()
+    print(m)
+    context={}
+    context['data']=m
+    return render(request,'dashboard.html', context)
+    #return HttpResponse("data fetched successfully")
+
+def delete(request, rid):
+    #print("id to be deleted:", rid)
+    m=Msg.objects.filter(id=rid)
+    m.delete()
+    return redirect('/dashboard')
+    #return HttpResponse("id to be deleted:"+rid)
+
+def edit(request, rid):
+    #print("id to be edit:", rid)
+    if request.method=='POST':
+        #update data
+        n=request.POST['uname']
+        mail=request.POST['uemail']
+        mob=request.POST['mobile']
+        msg=request.POST['msg'] 
+        #print(n)
+        #print(mail)
+        m=Msg.objects.filter(id=rid)
+        m.update(name=n,email=mail, mobile=mob,msg=msg)
+        return redirect('/dashboard')
+      # return HttpResponse('update')
+
+    else:
+        #display form with old data
+        m=Msg.objects.get(id=rid)    
+        context={}
+        context['data']=m
+        return render(request,'edit.html', context)
+        #return HttpResponse("id to be edited:"+rid)
+    
